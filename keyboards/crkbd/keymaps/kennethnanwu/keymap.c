72fe76a8f0579fe11419ac05d1082e6afff41869
Kenneth <huizhouwunan@hotmail.com>
2022-04-21 23:25:39 -0400

    Update keymap.c

--- keyboards/crkbd/keymaps/kennethnanwu/keymap.c
+++ keyboards/crkbd/keymaps/kennethnanwu/keymap.c
@@ -31,55 +31,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+-------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,   KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,                         KC_N,  KC_M,    KC_COMM, KC_DOT,RCTL_T(KC_SLSH),KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, KC_LGUI, LCTL_T(KC_SPC),LT(2,KC_ENT), TT(1),LT(3,KC_CAPS)
+                                          KC_LALT, KC_LGUI, LCTL_T(KC_SPC),KC_ENT, TT(1),KC_CAPS
                                       //`--------------------------'  `--------------------------'
 
   ),
 // KC_TRNS means transparent, required for MO()
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,   KC_Q,    KC_W,  KC_BTN1,   KC_R,    KC_T,                      KC_HOME,  KC_PGDN, KC_PGUP, KC_END,    KC_P, KC_QUOT,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC,   KC_A,    KC_S,  KC_BTN3,   KC_F,    KC_G,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_WH_D,  KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,   KC_Z,     KC_X,  KC_BTN2,  KC_V,    KC_B,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_WH_U, KC_BSLS,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, KC_LGUI, LCTL_T(KC_SPC),KC_ENT,KC_TRNS, KC_CAPS
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [2] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,    KC_8,     KC_9,    KC_0, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR, KC_PERC,                      KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,KC_BSPC,
+      KC_ESC,   KC_A,  KC_HOME,  KC_PGUP, KC_PGDN, KC_END,                      KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT,  KC_SCLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD,                       KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, KC_LGUI, LCTL_T(KC_SPC), LT(2,KC_ENT),KC_TRNS,KC_CAPS
-                                      //`--------------------------'  `--------------------------'
-  )
-  ,
-
-  [3] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO, KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_MOD, RGB_SAI, RGB_VAI, RGB_HUI, RGB_SPI,                        KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO,  RGB_RMOD, RGB_SAD, RGB_VAD, RGB_HUD, RGB_SPD,                      BL_TOGG, BL_BRTG, BL_INC,  BL_DEC,   BL_BRTG,  KC_NO,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO, LT(3,KC_CAPS)
+                                          KC_LALT, KC_LGUI, LCTL_T(KC_SPC),KC_ENT,KC_TRNS, KC_CAPS
                                       //`--------------------------'  `--------------------------'
   )
 };
 
 const rgblight_segment_t PROGMEM layer_navi[] = RGBLIGHT_LAYER_SEGMENTS(
-    {1, 20, HSV_GREEN}
+    {1, 21, HSV_GREEN}
 );
 
 const rgblight_segment_t PROGMEM layer_caps[] = RGBLIGHT_LAYER_SEGMENTS(
-    {1, 20, HSV_RED}
+    {22, 42, HSV_RED}
 );
 
 // Now define the array of layers. Later layers take precedence
