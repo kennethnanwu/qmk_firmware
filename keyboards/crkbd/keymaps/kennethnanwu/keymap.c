90f96c4c8c01d66d1506160ec51ad651a7bf0958
Kenneth <huizhouwunan@hotmail.com>
2022-04-22 11:59:28 -0400

    Update keymap.c
    layer 1 semi colon key should also be shift when long press

--- keyboards/crkbd/keymaps/kennethnanwu/keymap.c
+++ keyboards/crkbd/keymaps/kennethnanwu/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,    KC_8,     KC_9,    KC_0, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC,   KC_A,  KC_HOME,  KC_PGUP, KC_PGDN, KC_END,                      KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT,  KC_SCLN, KC_BSPC,
+      KC_ESC,   KC_A,  KC_HOME,  KC_PGUP, KC_PGDN, KC_END,                      KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT,RSFT_T(KC_SCLN),KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TILD,                       KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
