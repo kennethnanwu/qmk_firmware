1922e83b422404f20bc6abb5669b7c9b6fa92004
Nan <huizhouwunan@hotmail.com>
2022-09-12 21:07:29 -0400

    right ctrl and sft matching left positions

--- keyboards/crkbd/keymaps/kennethnanwu/keymap.c
+++ keyboards/crkbd/keymaps/kennethnanwu/keymap.c
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  LCTL_T(KC_ESC),KC_A,   KC_S,    KC_D,    KC_F,   KC_G,                         KC_H,    KC_J,    KC_K,  KC_L, RSFT_T(KC_SCLN),KC_QUOT,
+  LCTL_T(KC_ESC),KC_A,   KC_S,    KC_D,    KC_F,   KC_G,                         KC_H,    KC_J,    KC_K,  KC_L, RCTL_T(KC_SCLN),RCS_T(KC_QUOT),
   //|--------+--------+--------+--------+-------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,  KC_M,    KC_COMM, KC_DOT,RSFT_T(KC_SLSH),KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          KC_LALT, KC_LGUI,  KC_SPC,     KC_ENT,   MO(2),  TG(1)
                                       //`--------------------------'  `--------------------------'
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,     KC_7,    KC_8,     KC_9,    KC_0, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,                       KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, KC_TRNS, KC_QUOT,
+     KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,                       KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, KC_TRNS,RCS_T(KC_QUOT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_TRNS, KC_PLUS, KC_UNDS, KC_LCBR, KC_RCBR, KC_TILD,                       KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL, KC_TRNS,
+     KC_TRNS, KC_PLUS, KC_UNDS, KC_LCBR, KC_RCBR, KC_TILD,                       KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS,RSFT_T(KC_EQL), KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
