d1be9475fb8eaf128c86d79436c25fba33101934
Nan <huizhouwunan@hotmail.com>
2022-09-20 23:46:27 -0400

    right pinky also toggle movement layer

--- keyboards/crkbd/keymaps/kennethnanwu/keymap.c
+++ keyboards/crkbd/keymaps/kennethnanwu/keymap.c
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  LCTL_T(KC_ESC),KC_A,   KC_S,    KC_D,    KC_F,   KC_G,                         KC_H,    KC_J,    KC_K,  KC_L, RCTL_T(KC_SCLN),RCS_T(KC_QUOT),
+  LCTL_T(KC_ESC),KC_A,   KC_S,    KC_D,    KC_F,   KC_G,                         KC_H,    KC_J,    KC_K,  KC_L,LT(2, KC_SCLN),RCTL_T(KC_QUOT),
   //|--------+--------+--------+--------+-------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,  KC_M,    KC_COMM, KC_DOT,RSFT_T(KC_SLSH),KC_BSLS,
+     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,  KC_M,    KC_COMM, KC_DOT,RCS_T(KC_SLSH),RSFT_T(KC_BSLS),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          KC_LALT, KC_LGUI,  KC_SPC,     KC_ENT,   MO(2),  TG(1)
                                       //`--------------------------'  `--------------------------'
