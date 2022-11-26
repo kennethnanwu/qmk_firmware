ba9d23a26ede54958e650a101e07f65d61457e60
Nan <huizhouwunan@hotmail.com>
2022-11-25 22:52:27 -0500

    revert back to pinky shift and control

--- keyboards/crkbd/keymaps/kennethnanwu/keymap.c
+++ keyboards/crkbd/keymaps/kennethnanwu/keymap.c
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_ESC,KC_A,KC_S,LSFT_T(KC_D),LCTL_T(KC_F),   KC_G,                        KC_H,RCTL_T(KC_J),RSFT_T(KC_K),KC_L,LT(1, KC_SCLN),RCTL(KC_TAB),
+     KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,KC_L,LT(1, KC_SCLN),RCTL(KC_TAB),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_CAPS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,RCS(KC_TAB),
+     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_CAPS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          KC_LALT, KC_LGUI,  KC_SPC,     KC_ENT,   MO(2),  TG(1)
                                       //`--------------------------'  `--------------------------'
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // KC_TRNS means transparent, required for MO()
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,     KC_7,    KC_8,     KC_9,    KC_0, KC_TRNS,
+     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_TRNS, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR, KC_PERC,                      KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
